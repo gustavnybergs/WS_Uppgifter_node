736c7f35c5d98d0e308cb005d918cbb8e56eb517
gustavnybergs <Gustavnybergs@gmail.com>
2025-08-25 09:26:14 +0200

    fix: Uppdaterar startscript till ESM-loader för ts-node

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port: number = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+// Viktigt: lägg listen sist när allt är uppsatt
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
